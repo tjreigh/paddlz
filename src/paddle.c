33012ecd1bc6bcf2b112e7ce670d678ea185eca1
Trevor R <tjdoescode@gmail.com>
2023-12-18 16:38:05 -0600

    fix: keep paddle within vertical screen boundaries

--- src/paddle.c
+++ src/paddle.c
@@ -16,47 +16,48 @@ paddle_t *initPaddle()
 	paddle->pos = init_pos;
 	paddle->is_colliding = false;
 
-	gfx_SwapDraw();
-	gfx_Wait();
+	gfx_SetDrawBuffer();
 
 	gfx_SetColor(BG_COLOR);
 	gfx_FillRectangle(0, 0, 240, 240);
 	gfx_SetColor(PADDLE_COLOR);
 	gfx_FillRectangle_NoClip(INIT_X_LOC, INIT_Y_LOC, PADDLE_WIDTH, PADDLE_LEN);
 
-	gfx_SwapDraw();
-	gfx_Wait();
+	gfx_BlitBuffer();
 
 	return paddle;
 }
 
 void movePaddle(paddle_t *paddle)
 {
-	gfx_SwapDraw();
-	gfx_Wait();
-
-	gfx_SetColor(BG_COLOR);
-	gfx_FillRectangle(0, 0, 240, 240);
-
 	int amnt = (paddle->move_dir == 0) ? -2 : 2;
 	int curr_pos = paddle->pos.y;
 	int new_pos = curr_pos + amnt;
 
 	dbg_sprintf(dbgout, "current paddle y pos is: %d\n new position would be: %d\n", curr_pos, new_pos);
 
-	gfx_SetColor(PADDLE_COLOR);
-
-	if (new_pos < 210 || new_pos > 1)
+	if (new_pos < 210 && new_pos > 1)
 	{
+		gfx_SetDrawBuffer();
+
+		// clear out display
+		gfx_SetColor(BG_COLOR);
+		gfx_FillRectangle(0, 0, 240, 240);
+
+		gfx_SetColor(PADDLE_COLOR);
+
 		gfx_FillRectangle_NoClip(INIT_X_LOC, new_pos, PADDLE_WIDTH, PADDLE_LEN);
+
 		dbg_sprintf(dbgout, "moving paddle to y pos: %d\n", new_pos);
 		paddle->pos.y = new_pos;
 	}
 	else
+	{
+		gfx_FillRectangle_NoClip(INIT_X_LOC, curr_pos, PADDLE_WIDTH, PADDLE_LEN);
 		paddle->should_move = false;
+	}
 
-	gfx_SwapDraw();
-	gfx_Wait();
+	gfx_BlitBuffer();
 }
 
 void updatePaddle(paddle_t *paddle)
